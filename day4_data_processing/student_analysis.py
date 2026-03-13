27a5e5c4a0b3ff951c54f8a59cd0e7668974781a
Daksh Chaudhary <dakshchaudhary@Dakshs-MacBook-Air.local>
2026-03-13 14:14:31 +0530

    made the program code more professional and updatd the README.md

--- day4_data_processing/student_analysis.py
+++ day4_data_processing/student_analysis.py
@@ -7,3 +7,19 @@ students = [
 
 for student in students:
     print(student['name'] + " - " + str(student['marks']))
+
+max_marks = 0
+total_marks = 0
+topper = ""
+for student in students:
+    total_marks += student['marks']
+    if(student['marks'] > max_marks):
+        max_marks = student['marks']
+        topper = student['name']
+
+print(f"Topper: {topper} with marks {max_marks}")
+
+print(f"Average Marks: {(total_marks / len(students))}")
+
+
+
